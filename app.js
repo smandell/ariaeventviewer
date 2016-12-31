c212567d00bc3db5b3b055583cbc66994b8c65bb
Samuel Mandell <shmandell@gmail.com>
2016-12-31 14:28:41 -0700

    attempting to use XML parser

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var xmlparser = require('express-xml-bodyparser');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,11 +18,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.text());
+app.use(bodyParser.raw());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(xmlparser());
 app.use('/', routes);
 app.use('/users', users);
 
