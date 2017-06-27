e585fde6ff47cc5f7747139a62ac6c9cb88ae182
Samuel Mandell <shmandell@gmail.com>
2017-06-27 05:34:48 -0600

    removing references to users

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -62,7 +59,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-app.use('/users', users);
 
 
 // catch 404 and forward to error handler
